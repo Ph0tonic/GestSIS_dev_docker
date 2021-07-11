7d8353f979ebc5715a966f8abbe9ea0c60b92e72
bastien wermeille <bastien.wermeille@gmail.com>
2021-07-11 16:45:45 +0200

    Update multiple DB pour API

--- docker-compose/mysql/gestsis.sql
+++ docker-compose/mysql/gestsis.sql
@@ -3,10 +3,16 @@
 
 -- # Create second_db database if it doesn't exist
 DROP DATABASE IF EXISTS gestsis;
+DROP DATABASE IF EXISTS hs;
+DROP DATABASE IF EXISTS test;
 DROP DATABASE IF EXISTS gestsis_auth;
 CREATE DATABASE IF NOT EXISTS gestsis;
+CREATE DATABASE IF NOT EXISTS hs;
+CREATE DATABASE IF NOT EXISTS test;
 CREATE DATABASE IF NOT EXISTS gestsis_auth;
 
 -- # Grant all privilidges on second_db to org_user
 GRANT ALL PRIVILEGES ON gestsis_auth.* TO 'gestsis'@'%';
 GRANT ALL PRIVILEGES ON gestsis.* TO 'gestsis'@'%';
+GRANT ALL PRIVILEGES ON hs.* TO 'gestsis'@'%';
+GRANT ALL PRIVILEGES ON test.* TO 'gestsis'@'%';
