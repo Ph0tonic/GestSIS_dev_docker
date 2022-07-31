7e8b36a2255e6d2191d856cd318f95de8d868d04
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-07-31 16:40:00 +0200

    Update config for GestSIS Alarm

--- config_alarm.sh
+++ config_alarm.sh
@@ -1,8 +1,8 @@
 # GestSIS_Alarm
 
 # Initialisation du fichier de config
-cp ./GestSIS_Alarm/gestsis_alarm/.env.docker ./GestSIS_Alarm/gestsis_alarm/.env
+cp ./GestSIS_Alarm/.env.docker ./GestSIS_Alarm/.env
 
 # Config des clés pour GestSIS API
-mkdir ./GestSIS_Alarm/gestsis_alarm/storage/keys
-cp ./rsa_keys/auth-public.key ./GestSIS_Alarm/gestsis_alarm/storage/keys/auth-public.key
+mkdir ./GestSIS_Alarm/storage/keys
+cp ./rsa_keys/auth-public.key ./GestSIS_Alarm/storage/keys/auth-public.key
